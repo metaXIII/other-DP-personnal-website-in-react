510df5e9921080b7614bbcaebaf0956ec27e3b55
DavidPoulain <david.poulain60@gmail.com>
2022-07-06 16:30:30 +0200

    trying some ideas for portfolio

---
+++ src/components/Portfolio/PortfolioOtherElement/index.js
@@ -0,0 +1,9 @@
+import './portfolioOtherElement.scss';
+
+const PortfolioOtherElement = () => (
+  <div className="PortfolioElement">
+    <h3>Follow Your Party</h3>
+  </div>
+);
+
+export default PortfolioOtherElement;
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,17 +1,23 @@
 import PortfolioElement from 'src/components/Portfolio/PortfolioElement';
+import PortfolioOtherElement from 'src/components/Portfolio/PortfolioOtherElement';
+import Viewpager from 'src/components/Portfolio/Viewpager';
 
 import './portfolio.scss';
 
 const Portfolio = () => (
-  <section id="portfolio">
-    <h2 className="title_section">Portfolio</h2>
+  <section className="section_portfolio" id="portfolio">
+    <h2 className="title_section">&#91; Portfolio &#93;</h2>
     <div className="container_flex">
+      {/* <Viewpager /> */}
       <PortfolioElement />
       <PortfolioElement />
       <PortfolioElement />
       <PortfolioElement />
       <PortfolioElement />
-      <PortfolioElement />
+      {/* <PortfolioOtherElement />
+      <PortfolioOtherElement />
+      <PortfolioOtherElement />
+      <PortfolioOtherElement /> */}
     </div>
   </section>
 );
