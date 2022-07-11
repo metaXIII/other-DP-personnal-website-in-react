4fbf363b634d8454d8b85c7882231770c68ed785
DavidPoulain <david.poulain60@gmail.com>
2022-07-11 13:23:00 +0200

    building the new card and beginning to animate

--- src/components/Portfolio/BoxElement/index.js
+++ src/components/Portfolio/BoxElement/index.js
@@ -1,13 +1,25 @@
 import './boxElement.scss';
 
-import FYP from 'src/assets/images/FYP.png';
+import FYP from 'src/assets/images/FYPcard.png';
 
 const BoxElement = () => (
   <article className="card">
-    <div className="card--front">
-      <img src={FYP} alt="Follow Your Party" />
+    <div className="card_front">
+      <img className="card_front--image" src={FYP} alt="Follow Your Party" />
+      <h3 className="card--name">Follow Your Party</h3>
+      <p className="card--date">Juin 2022</p>
     </div>
-    <div className="card--back"></div>
+    <div className="card_back">
+      <div className="card_back--info">
+        <p className="role">Product Owner, Lead Dev Front</p>
+        <p className="client">Projet de fin de formation</p>
+      </div>
+      <button className="card_back--buton" type="button">Voir le projet</button>
+      <div className="card_back--techno">
+        <p className="technoList">React.JS, Redux, Symfony</p>
+      </div>
+    </div>
+    <div className="card_background" />
   </article>
 );
 
